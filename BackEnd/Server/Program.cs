9898c478fb5d91a600e5149cd0efb9a2e25267f1
JasonVolney <jasvol@gmail.com>
2020-11-23 22:16:31 -0300

    Initial commit

---
+++ BackEnd/Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Carometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao Carômetro!");
+        }
+    }
+}
