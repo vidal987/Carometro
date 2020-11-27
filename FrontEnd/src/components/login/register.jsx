f7d744081ed5d1770e838d0df3b6d4f0f2b83f2d
Artur Alves <suelen@MacBook-Pro-de-Suelen.local>
2020-11-27 06:46:16 -0300

    página de login

---
+++ FrontEnd/src/components/login/register.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import loginImg from "../../senai.png";
+
+export class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+            <div className="header">Entrar</div>
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <input type="text" name="username" placeholder="NI" />
+            </div>
+            <div className="form-group">
+              <input type="password" name="password" placeholder="Senha" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Entrar
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
