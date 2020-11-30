b19f28b6864b0c3d5b7a85e8d09437dad85b4799
ramos-r <bckymoon@hotmail.com>
2020-11-30 14:35:00 -0300

    Atualizacao na Tela Login

--- FrontEnd/src/components/login/login.jsx
+++ FrontEnd/src/components/login/login.jsx
@@ -16,10 +16,10 @@ export class Login extends React.Component {
           </div>
           <div className="form">
             <div className="form-group">
-              <input type="text" name="username" placeholder="NI" />
+              <input type="text" name="username" placeholder=" NI" />
             </div>
             <div className="form-group">
-              <input type="password" name="password" placeholder="Senha" />
+              <input type="password" name="password" placeholder=" Senha" />
             </div>
           </div>
         </div>
