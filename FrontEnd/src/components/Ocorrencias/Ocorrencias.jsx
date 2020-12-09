559a2a0536990863aa487abf9143e48840e8608e
ramos-r <bckymoon@hotmail.com>
2020-12-09 15:45:21 -0300

    Adicionados botões e card das ocorrencias, + alguns ajustes

--- FrontEnd/src/components/Ocorrencias/Ocorrencias.jsx
+++ FrontEnd/src/components/Ocorrencias/Ocorrencias.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import student from "../../img/student.jpg";
+import edit from "../../img/edit.png";
+import del from "../../img/delete.png";
 
 export class Ocorrencias extends React.Component {
     constructor(props) {
@@ -9,25 +11,63 @@ export class Ocorrencias extends React.Component {
     render() {
       return (
         <div className="ocorrencias-container">
-            <h1 className="nome-aluno">Nome Completo do Aluno</h1>
+          <div className="header-formandos">
+            <h1 className="nome-aluno">Rebecca da Silva Souza Ramos</h1>
+            <button className="btn-voltar-formandos"><Link to="/Turmas"></Link>Voltar</button>
+          </div>
           <div className="container-ocorrencias">
               <div className="photo-card">
                     <img src={student} className="ocorrencias-pic" />
               </div>
               <div className="card-info-aluno">
                 <div className="info-aluno">
-                    <h2>Telefone:</h2>
-                    <h2>E-mail:</h2>
-                    <h2>Idade:</h2>
-                    <h2>Turma: </h2>
-                    <h2>Matrícula:</h2>
+                    <p>Telefone:</p>
+                    <p>E-mail:</p>
+                    <p>Idade:</p>
+                    <p>Turma: </p>
+                    <p>Matrícula:</p>
                 </div>
                 <div className="dados-container">
-                    <span className="dados-aluno">(xx) 9 0000-0000</span>
+                  {/* Dados ficticios de aluno para exemplificar */}
+                    {/* <span className="dados-aluno">(xx) 9 0000-0000</span>
                     <span className="dados-aluno">aluno@senaisp.edu.br</span>
                     <span className="dados-aluno">18</span>
                     <span className="dados-aluno">E-115-1-2020-1DT/2DT</span>
-                    <span className="dados-aluno">000000000000</span>
+                    <span className="dados-aluno">000000000000</span> */}
+                </div>
+              </div>
+              <div className="buttons-ocorrencia">
+                      <div className="edit-delete-btn">
+                        {/* Aqui será adicionada a função de Editar a ocorrência */}
+                        <img src={edit}/>
+                      </div>
+                      <div className="edit-delete-btn">
+                        <img src={del}/>
+                        {/* Aqui será adicionada a função de Excluir a ocorrência */}
+                      </div>
+                    </div>
+              <h1 className="nome-aluno">Ocorrências</h1>
+              <div className="ocorrencias-area">
+                <div className="ocorrencia">
+                  <div className="card-ocorrencia">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <div className="buttons-ocorrencia">
+                      <div className="edit-delete-btn">
+                        {/* Aqui será adicionada a função de Editar a ocorrência */}
+                        <img src={edit}/>
+                      </div>
+                      <div className="edit-delete-btn">
+                        <img src={del}/>
+                        {/* Aqui será adicionada a função de Excluir a ocorrência */}
+                      </div>
+                    </div>
+                  <div className="dados-criacao">
+                    <h3>Criada:</h3>
+                    {/* Data de criação da ocorrência e usuário que a criou */}
+                    <p className="data-ocorrencia">25/11/2020</p>
+                    <p>Claudia Camusso</p>
+                  </div>
                 </div>
               </div>
           </div>
