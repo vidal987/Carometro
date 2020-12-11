3aa1e02fe23ec9ab03aa3db12dea8e4fb69f3c21
Jason Volney Silva <54520187+JasonVolney@users.noreply.github.com>
2020-12-11 15:11:41 -0300

    Include Header

--- FrontEnd/src/Turma/Turma.js
+++ FrontEnd/src/Turma/Turma.js
@@ -1,35 +1,37 @@
 import React from "react";
-import '../App.scss';
+import './Turma.css';
 import { Link } from 'react-router-dom';
+import { Header } from '../Header/Header.js';
 
-//Atenção!! Era o componente CadatrarTurma.jsx
 export class Turma extends React.Component {
     constructor(props) {
       super(props);
     }
     render() {
       return (
-        <div className="cturma-container">
-          <div className="top-container">
-            <h1 className="page-title">Cadastrar turma</h1>
-            <button className="btn-voltar"><Link to="./Home">Voltar</Link></button>
+         <div className="nav-container">
+           <Header/>
+          <div className="cturma-container">
+            <div className="top-container">
+              <h1 className="page-title">Cadastrar turma</h1>
+              <button className="btn-voltar"><Link to="./Home">Voltar</Link></button>
+            </div>
+            <div className="section-input-cturma">
+                <div className="form-cturma">
+                <span className="span-cturma">Nome</span>
+                    <input type="text" placeholder="Nome da turma" className="input-cturma"></input>
+                </div>
+                <div className="form-cturma">
+                    <span className="span-cturma">Curso</span>
+                    <input type="text" placeholder="Nome do curso" className="input-cturma"></input>
+                </div>
+            </div>
+            <div className="btn-section-turma">
+              <button className="btn-turma">Cadastrar</button>
+              <button className="btn-turma">Adicionar nova turma</button>
+            </div>
           </div>
-          <div className="section-input-cturma">
-              <div className="form-cturma">
-              <span className="span-cturma">Nome</span>
-                  <input type="text" placeholder="Nome da turma" className="input-cturma"></input>
-              </div>
-              <div className="form-cturma">
-                  <span className="span-cturma">Curso</span>
-                  <input type="text" placeholder="Nome do curso" className="input-cturma"></input>
-              </div>
-          </div>
-          <div className="btn-section-turma">
-            <button className="btn-turma">Cadastrar</button>
-            <button className="btn-turma">Adicionar nova turma</button>
-          </div>
-        </div>
+         </div> 
         );
     }
 }
-
