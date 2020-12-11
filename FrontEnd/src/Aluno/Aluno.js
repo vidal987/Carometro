72486b523cede959eb6213557b80b6ba4f445232
Jason Volney Silva <54520187+JasonVolney@users.noreply.github.com>
2020-12-11 15:15:41 -0300

    Include Header

--- FrontEnd/src/Aluno/Aluno.js
+++ FrontEnd/src/Aluno/Aluno.js
@@ -3,10 +3,10 @@ import '../App.scss';
 import 'react-dropzone-uploader/dist/styles.css';
 import Dropzone from 'react-dropzone-uploader';
 import { Link } from 'react-router-dom';
+import { Header } from '../Header/Header.js';
 
-//Atenção!! Era o componente Cadatro.jsx
 export class Aluno extends React.Component {
-  
+
   constructor(props) {
     super(props);
 
@@ -41,66 +41,71 @@ export class Aluno extends React.Component {
 
   render() {
     return (
-      <div className="containercadastro">
-        <button type="button" className="btn2">
+      <div className="cadastro">
+        <Header />
+        <div className="container">
+          <button type="button" className="btn2">
             <Link to="/" style={{ color: '#FFF', textDecoration: 'none' }}>Voltar</Link></button>
-        <div className="content">
+          <div className="content">
             <div className="form">
-                <div className="cadastro">
-                    <h1>Cadastrar usuário</h1> 
-                        <label>Nome completo</label>                
-                        <input type="text" name="username" placeholder="Nome Completo" />
-                        <label>NI</label>
-                        <input type="text" name="NI" placeholder="NI" />      
-                        <label1>Senha</label1>
-                        <input type="password" name="password" placeholder="Senha" />          
-                </div>
-                <div className="acesso">
-                    <h3>Acesso</h3>
-                    <form onChange={this.handleFormChange}>
-                      <label className="label">
-                        <input 
-                        type="radio" 
-                        value="1" 
-                        name="name" 
-                        style={{ width: 16, height: 16 }}
-                        onChange={this.handleChange}
-                        onClick={this.handleClick}
-                        id="checkAdmin"   
-                        />
+              <div className="cadastro">
+                <h1>Cadastrar usuário</h1>
+                <label>Nome completo</label>
+                <input type="text" name="username" placeholder="Nome Completo" />
+                <label>NI</label>
+                <input type="text" name="NI" placeholder="NI" />
+                <label1>Senha</label1>
+                <input type="password" name="password" placeholder="Senha" />
+              </div>
+              <div className="acesso">
+                <h3>Acesso</h3>
+                <form onChange={this.handleFormChange}>
+                  <label className="label">
+                    <input
+                      type="radio"
+                      value="1"
+                      name="name"
+                      style={{ width: 16, height: 16 }}
+                      onChange={this.handleChange}
+                      onClick={this.handleClick}
+                      id="checkAdmin"
+                    />
                       Admin
                       </label>
-                      <label className="label">
-                        <input 
-                        type="radio" 
-                        value="2" 
-                        name="name" 
-                        style={{ width: 16, height: 16 }}
-                        onChange={this.handleChange}
-                        onClick={this.handleClick}
-                        id="checkTeacher" 
-                        /> 
+                  <label className="label">
+                    <input
+                      type="radio"
+                      value="2"
+                      name="name"
+                      style={{ width: 16, height: 16 }}
+                      onChange={this.handleChange}
+                      onClick={this.handleClick}
+                      id="checkTeacher"
+                    />
                       Docente
                       </label>
-                    </form>
-                </div>
+                </form>
+              </div>
 
-                <Dropzone/>
-                <button type="button" className="btn3">
-                    <Link to="./" style={{ color: '#FFF', textDecoration: 'none' }}>Finalizar</Link>
-                </button>
-            <button type="button" className="btn4">
-            <Link to="./Aluno" style={{ color: '#FFF', textDecoration: 'none' }}>Adicionar novo</Link> </button>
-      </div> 
-  </div>
-</div>
- );
- }
+              <Dropzone />
+              <button type="button" className="btn3">
+                <Link to="./" style={{ color: '#FFF', textDecoration: 'none' }}>Finalizar</Link>
+              </button>
+              <button type="button" className="btn4">
+                <Link to="./Aluno" style={{ color: '#FFF', textDecoration: 'none' }}>Adicionar novo</Link> </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Aluno.propTypes = {};
 
 Aluno.defaultProps = {}
+
+
 
 
 
