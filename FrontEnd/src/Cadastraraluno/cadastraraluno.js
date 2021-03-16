7bf17f152d5c78c7b9ce48e4612e6758007ef51f
ramos-r <bckymoon@hotmail.com>
2021-03-16 15:54:39 -0300

    Adicionei novamente as Div's dos forms, não está completo.

--- FrontEnd/src/Cadastraraluno/cadastraraluno.js
+++ FrontEnd/src/Cadastraraluno/cadastraraluno.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './cadastraraluno.css';
+import estilos from './CadastrarAluno.module.css';
 import { Link } from 'react-router-dom';
 import Dropzone from 'react-dropzone-uploader';
 import add from '../img/add.png';
@@ -7,59 +7,72 @@ import aluno from '../img/aluno.png';
 import list from '../img/list.png';
 import turma from '../img/turma.png';
 import { Header } from '../Header/Header.js';
+import { Fragment } from "react";
 
 export class cadastraraluno extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-            return (
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="nav-container1">
+        <Header />
+        <div className={estilos["container-cadastroRe"]}>
+          <div className={estilos["top-container1"]}>
+            <h1 className={estilos["page-title1"]}>Cadastrar Aluno</h1>
+            <Link to="/Cadastrar">
+              <button className={estilos["btn-voltar1"]}>Voltar</button>
+            </Link>
+          </div>
+          <div className="form-groupRe">
+            <div className={estilos["formRe"]}>
+              <label>Nome completo</label>
+              <input className={estilos["input-cadastro"]} type="text" placeholder="Nome Completo" />
+            </div> 
 
-                <div className="nav-container">
-                <Header/>
-                <div className="home-container">
-                  <div className="container-homebtn">
-                  <div className="form">                          
-                                <h1>Cadastrar Aluno</h1> 
-                                    <label2>Nome completo</label2> 
-                                    <input type="text" name="username" placeholder="Nome Completo" />
-                                
-                                    <div className="cadastroaluno2">
-                                    <label3>Turma</label3>
-                                    <label4>Matricula</label4>
-                                    
-                                    <input type="text" name="Turma" placeholder="Turma" />   
-                                    <input type="text" name="Matrícula" placeholder="Matrícula" />    
-                                  
-                                    <label5>Telefone</label5>
-                                    <label6>Celular</label6>
-                                    <br></br>
-                                    <input type="text" name="Turma" placeholder="Telefone" />      
-                                    <input type="text" name="Matrícula" placeholder="(00) 00000-0000" />  
-                                  
-                                    <label7>E-mail</label7>
-                                    <label8>Data de Nascimento</label8>
-                                    <br></br> 
-                                    <input type="text" name="Turma" placeholder="E-mail" />      
-                                    <input type="text" name="Matrícula" placeholder="01/01/2001" />                                   
-                            <Dropzone/>
-                            <br></br>
-                            <button type="button" className="btn-end">
-                                <Link to="./" style={{ color: '#FFF', textDecoration: 'none' }}>Finalizar</Link>
-                            </button>
-                        <button type="button" className="btn-add-new">
-                        <Link to="./Aluno" style={{ color: '#FFF', textDecoration: 'none' }}>Adicionar novo</Link> </button>
-                            </div>
+            <div className={estilos["formRe"]}>
+              <label>Turma</label>
+              <input className={estilos["input-cadastro"]} type="text" placeholder="Turma" />
+            </div>
 
-                  </div> 
-                  </div>
-                </div>
-                </div>
+            <div className={estilos["formRe"]}> 
+              <label>Matricula</label>
+              <input className={estilos["input-cadastro"]} type="text" placeholder="Matrícula" />
+            </div>
 
+            <div className={estilos["formRe"]}>
+              <label>Telefone</label>
+              <input className={estilos["input-cadastro"]} type="text" placeholder="Telefone" />
+            </div>
 
+            <div className={estilos["formRe"]}>
+              <label>Celular</label>
+              <input className={estilos["input-cadastro"]} type="text" placeholder="(00) 00000-0000" />
+            </div>
 
+            <div className={estilos["formRe"]}>
+              <label>E-mail</label>
+              <input type="text" name="Turma" placeholder="E-mail" />
+            </div>
 
-                
+            <div className={estilos["formRe"]}>
+              <label>Data de Nascimento</label><br></br>
+              <input type="date" name="Matrícula"/>
+            </div>
+
+            <div className={estilos["formRe"]}>
+            <label>Foto do Aluno</label>
+            <Dropzone />
+            </div>
+
+            <div className={estilos["bot-container1"]}>
+              <button type="button" className={estilos["btn-end"]}>Finalizar</button>
+              <button type="button" className={estilos["btn-add-new"]}>Adicionar novo</button>
+            </div>
+            
+          </div>
+        </div>
+      </div>
          );
-         }
-        }
+  }
+}
